a40eb94a8d1ce28ee75d941113a6b82323b31e39
heshan <heshandinushka98@gmail.com>
2023-03-22 08:23:01 +0530

    initialize

---
+++ src/main/java/com/bumblebee/heshan/HeshanApplication.java
@@ -0,0 +1,13 @@
+package com.bumblebee.heshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeshanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeshanApplication.class, args);
+	}
+
+}
